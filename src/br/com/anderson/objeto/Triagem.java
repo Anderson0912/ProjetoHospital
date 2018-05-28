c97ab6ed1d833f4acffc977fb7d86bbd3b4a6e21
SATC <SATC@L-1-18-18--10>
2018-05-28 17:18:36 -0300

    Criado interface para cadastro de pacientes, médicos e enfermeiras de um hospital.

---
+++ src/br/com/anderson/objeto/Triagem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.anderson.objeto;
+
+import java.util.List;
+
+/**
+ *
+ * @author SATC
+ */
+public class Triagem {
+    private Enfermeira enfermeira;
+    private Paciente paciente;
+    private String pressao, sintoma;
+    private float temperatura, peso, altura, imc;
+    private List<String> alergias;
+    private boolean febre;
+}
